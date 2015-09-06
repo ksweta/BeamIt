498812baa53116206c7a315fa6e5513a235d9628
Kumari Sweta <ksweta1007@gmail.com>
2015-09-06 00:54:56 -0700

    Added code for sending and receiving NDEF messages

--- app/src/main/java/com/contactsharing/beamit/DataSetChange.java
+++ app/src/main/java/com/contactsharing/beamit/DataSetChange.java
@@ -1,6 +1,7 @@
 package com.contactsharing.beamit;
 
 /**
+ * This interface informs implementor(whoeve implements this interface) that data has been changed.
  * Created by kumari on 5/18/15.
  */
 public interface DataSetChange {
